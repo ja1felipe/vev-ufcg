84b6ed551ffd902be9378936c0425fb9ad5522fe
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2022-10-24 13:47:17 -0300

    feat: add third test red

--- nota-fiscal/test/deve_gerar_nota_fiscal_test.py
+++ nota-fiscal/test/deve_gerar_nota_fiscal_test.py
@@ -26,3 +26,16 @@ def test_gera_nota_fiscal_25_imposto():
   nf = gerador_nf.execute(fatura)
   assert nf.valor == valor_fatura
   assert nf.imposto == valor_fatura * 0.25
+
+def test_gera_nota_fiscal_15_imposto():
+  nome_cliente = "Carlos"
+  endereco_cliente = "Rua A"
+  tipo_servico = TipoFatura.CONSULTORIA
+  valor_fatura = 1000
+  fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
+
+  gerador_nf = GeradorNF()
+
+  nf = gerador_nf.execute(fatura)
+  assert nf.valor == valor_fatura
+  assert nf.imposto == valor_fatura * 0.15
